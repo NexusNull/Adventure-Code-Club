940f5ad48b87e77ae053f29a86030d604dea0cfe
NexusNull <p.wellershaus@googlemail.com>
2019-07-03 17:44:59 +0200

    add UI

--- tracking/script.js
+++ tracking/script.js
@@ -1,10 +1,10 @@
 let ACC = function () {
-    this.asd = "gay";
     this.kills = {};
     this.chests = {};
     this.debug = false;
     this.apiKey = "7ddf32e17a6ac5ce04a8ecbf782ca509";
     this.dropServer = "drop.adventurecode.club"
+    this.UI = new ACCUI(this);
 };
 
 ACC.prototype.registerListeners = function () {
@@ -40,7 +40,7 @@ ACC.prototype.sendUpgradeData = function(data){
         data: data,
     };
     request.send(JSON.stringify(post));
-};i
+};
 
 ACC.prototype.sendCompoundData = function(data){
     let request = new XMLHttpRequest();
@@ -56,6 +56,7 @@ ACC.prototype.sendCompoundData = function(data){
 };
 ACC.prototype.setup = function () {
     this.registerListeners();
+    this.UI.addACCMenuButton();
 };
 
 ACC.prototype.detectGrouping = function () {
@@ -64,6 +65,35 @@ ACC.prototype.detectGrouping = function () {
 
 ACC.prototype.submitData = function(){
 
+};
+
+ACCUI = function(){
+    this.menu = null;
+
+};
+
+ACCUI.prototype.addACCMenuButton = function(){
+    var self = this;
+    let menuOpener = document.createElement("div");
+    menuOpener.innerHTML = "ACC";
+    menuOpener.setAttribute("class","gamebutton");
+    menuOpener.addEventListener("click", self.openMenu.bind(self));
+    let topRightCorner = document.getElementById("toprightcorner");
+    topRightCorner.insertBefore(menuOpener, topRightCorner.children[1]);
+};
+
+ACCUI.prototype.openMenu = function(){
+    if(!this.menu){
+        let menu = document.createElement("div");
+        this.menu = menu;
+        menu.setAttribute("class","modal");
+        menu.setAttribute("style","position: fixed; top: 0px; bottom: 0px; left: 0px; right: 0px; z-index: 9000; text-align: center; vertical-align: middle; overflow-y: scroll; background: rgba(0,0,0,0.4)")
+        menu.innerHTML = "<div>hello there</div>";
+        menu.addEventListener("click",function(){menu.parentNode.removeChild(menu)});
+        document.body.append(menu);
+    } else {
+        document.body.append(this.menu);
+    }
 };
 
 if (!window.ACC) {
